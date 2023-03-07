9b88f37414334828ab5cdd51f207e75855d5bbee
Rok Mihevc <rok@mihevc.org>
2023-03-07 23:44:38 +0100

    Add flight compression

--- test/test_expand_slot.py
+++ test/test_expand_slot.py
@@ -4,3 +4,11 @@ from ssim import ssim
 def test_expand_slot(expanding_slots):
     for slot in expanding_slots:
         assert ssim._expand(slot["slot"]) == slot["flights"]
+
+
+def test_roundtrip_slots(expanding_slots):
+    for slot in expanding_slots:
+        flights = ssim.expand_slots([slot["slot"]])
+
+        assert slot["flights"] == flights
+        assert slot["slot"] == ssim.compress_flights(flights)[0]
